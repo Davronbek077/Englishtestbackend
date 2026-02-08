17a00ad7dd462eabb3de67a69911b5de46edf340
wwecoder15 <rahmonovdavronbek770@gmail.com>
2026-02-08 14:21:23 +0500

    server bugs fixed

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
+const testRoutes = require("./routes/testRoutes");
+const questionRoutes = require("./routes/QuestionRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/tests", testRoutes);
+app.use("/api/questions", questionRoutes);
 
 const PORT = process.env.PORT || 5000;
 
